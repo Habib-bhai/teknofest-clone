138bb94d6a68f8765bffc740f9419485665bf67a
Habib-bhai <habibahmed918131@gmail.com>
2024-10-15 01:01:06 +0500

    add footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import About from "./pages/About"
 import Contact from "./pages/Contact"
 import Competitions from "./pages/Competitions"
 import CompetitionDetails from "./pages/CompetitionDetails"
+import Footer from "./components/Footer"
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
     <Route path="/competitions:competitionDetails" element={<CompetitionDetails/>} />
      </Routes>
 
+     <Footer/> 
     </>
   )
 }
--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,13 +1,41 @@
-function Footer() {
-  return (
-    <>
-    <div>
+import { NavLink } from "react-router-dom"
 
-        
-    </div>
-    
-    </>
-  )
+function Footer() {
+    return (
+        <>
+            <div className="w-full mt-8 flex flex-col justify-center items-center bg-gradient-to-r from-[#333970] to-[#325632]">
+                <img src="/AMP.png" alt="logo" className="w-48 h-48 rounded-full mt-10" />
+
+                <h1 className="text-3xl font-bold text-center mt-6 text-white">Empowering innovation, igniting future leaders. Teknofest Pakistan – where technology meets limitless possibilities</h1>
+
+                <div className="flex justify-center items-center gap-3 mt-10">
+
+                    <img src="/facebook.png" alt="" className="w-10 h-10 p-3 bg-white/40 rounded-full" />
+                    <img src="/instagram.png" alt="" className="w-10 h-10 p-3 bg-white/40 rounded-full" />
+                    <img src="youtube.png" alt="" className="w-10 h-10 p-3 bg-white/40 rounded-full" />
+                    <img src="linkedin.png" alt="" className="w-10 h-10 p-3 bg-white/40 rounded-full " />
+                </div>
+
+                <p className="h-[1px] bg-white/20  w-[50%] mt-10"></p>
+
+                <div className="flex justify-center items-center gap-8 mt-10 text-white text-xl font-bold">
+                    <NavLink to={"/"} className={({ isActive }) => isActive ? "text-[#388636]" : ""}>Home</NavLink>
+
+                    <NavLink to={"/about"} className={({ isActive }) => isActive ? "text-[#388636]" : ""}>About Us</NavLink>
+
+                    <NavLink to={"/competitions"} className={({ isActive }) => isActive ? "text-[#388636]" : ""}>Competitions</NavLink>
+
+                    <NavLink to={"/contact"} className={({ isActive }) => isActive ? "text-[#388636]" : ""}>Contact Us</NavLink>
+                </div>
+
+              <h1 className="text-white mt-10 text-xl font-bold">Copyright ©2023 TeknoFest All Rights Reserved Copyright</h1>  
+
+              <h1 className="text-white mt-10 text-xl font-bold">Powerd By: <span className="text-[#388636]"> workup solutions</span></h1>
+
+            </div>
+
+        </>
+    )
 }
 
 export default Footer
